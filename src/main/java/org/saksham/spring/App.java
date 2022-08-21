14151327ad00c00ac1ea94f932ff9dead28a94e6
Saksham Gupta <saksham757gupta@gmail.com>
2022-08-21 09:27:56 +0530

    Part1

--- src/main/java/org/saksham/spring/App.java
+++ src/main/java/org/saksham/spring/App.java
@@ -1,11 +1,16 @@
 package org.saksham.spring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        new Student();
+//        System.out.println("Hello World!");
+        ApplicationContext context = new ClassPathXmlApplicationContext("file:src/main/java/config.xml");
+        Student student = (Student) context.getBean("student1");
+        System.out.println(student);
     }
 }
